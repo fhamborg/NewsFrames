1f96c9c82f4d145114206eec50d00eda4fb1ac5e
Felix Hamborg <fhamborg@users.noreply.github.com>
2023-10-17 17:51:57 +0200

    init

--- src/NewsFrames/classifier.py
+++ src/NewsFrames/classifier.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Iterable, Union
 
 from loguru import logger
 
@@ -9,9 +9,9 @@ class Classifier:
     ECON = "ECON"
     GOV = "GOV"
 
-    DIMENSIONS = [AFF, CULT, ECON, GOV]
+    DIMENSIONS = (AFF, CULT, ECON, GOV)
 
-    def __init__(self, dimensions: List[str] = None):
+    def __init__(self, dimensions: List[str] = DIMENSIONS):
         for dimension in dimensions:
             assert (
                 dimension in Classifier.DIMENSIONS
@@ -21,10 +21,22 @@ class Classifier:
 
         logger.info("Created classifier with dimensions {}", self.dimensions)
 
-    def predict(self, sentences: List[str]):
+    def load_models(self):
+        return {}
+
+    def predict(self, sentences: Union[Iterable[str], str]):
         """
 
         :param sentences:
         :return:
         """
+        if isinstance(sentences, str):
+            sentences = [sentences]
+        for i, sentence in enumerate(sentences):
+            assert isinstance(
+                sentence, str
+            ), f"Expected sentences to be str, got {type(sentence)} for {i}-th item"
+
         return ["hi"] * len(sentences)
+
+
