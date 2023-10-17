220ed14f1abaa4035cf989dc4c06ff9a28b0ae30
Felix Hamborg <fhamborg@users.noreply.github.com>
2023-10-17 17:21:34 +0200

    init

---
+++ src/NewsFrames/classifier.py
@@ -0,0 +1,25 @@
+from typing import List
+
+from loguru import logger
+
+
+class Classifier:
+    DIMENSIONS = ["AFF", "CULT", "ECON", "GOV"]
+
+    def __init__(self, dimensions: List[str] = None):
+        for dimension in dimensions:
+            assert (
+                dimension in Classifier.DIMENSIONS
+            ), f"Dimension {dimension} not in {Classifier.DIMENSIONS}"
+        self.dimensions = dimensions
+        self.dim2model = self.load_models()
+
+        logger.info("Created classifier with dimensions {}", self.dimensions)
+
+    def predict(self, sentences: List[str]):
+        """
+
+        :param sentences:
+        :return:
+        """
+        return ["hi"] * len(sentences)
